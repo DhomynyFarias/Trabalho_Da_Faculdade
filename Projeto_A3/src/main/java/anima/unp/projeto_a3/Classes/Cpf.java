a78106481dc8f374ed453ebb962ea89e686904d9
DhomynyFarias <66629307+DhomynyFarias@users.noreply.github.com>
2023-05-10 17:41:35 -0300

    criação do projeto
    Foi criado as pastas do projeto, e uma classe cpf, que irá conter o método validarCpf(), assim que estiver concluída, irá cumprir um dos requisitos

---
+++ Projeto_A3/src/main/java/anima/unp/projeto_a3/Classes/Cpf.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package anima.unp.projeto_a3.Classes;
+
+/**
+ *
+ * @author geral
+ */
+public class Cpf {
+    String cpf;
+    
+    public void validarCpf(){
+        cpf = "função em construção";
+    }
+    
+    
+}
---
+++ Projeto_A3/src/main/java/anima/unp/projeto_a3/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package anima.unp.projeto_a3;
+
+/**
+ *
+ * @author geral
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
